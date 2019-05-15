7a8e6e313871496525b44f465cd2d6790be61502
Martin Hartman <hartmanx1@wp.pl>
2019-05-16 00:50:44 +0200

    first commit

---
+++ soloproject_end/js/script.js
@@ -0,0 +1,30 @@
+var hamburger = document.querySelector('.toggle-menu');
+var sidebar = document.querySelector('.sidebar');
+var menager = document.querySelector('.menager');
+var michael = document.querySelector('.men');
+var ajax = document.querySelectorAll('.ajax');
+
+var logo = document.querySelector('.logo');
+var photo = document.querySelector('.photo');
+var icon = document.querySelectorAll('.icon');
+
+
+hamburger.addEventListener('click', function(){
+
+	for(var i = 0; i < ajax.length; i++){
+		ajax[i].classList.toggle('visible');
+	};
+
+	for(var i = 0; i < icon.length; i++){
+		
+		icon[i].classList.toggle('visible');
+	};
+
+	menager.classList.toggle('visible');
+	photo.classList.toggle('visible');
+	hamburger.classList.toggle('visible');
+	logo.classList.toggle('visible');
+	sidebar.classList.toggle('visible');
+	michael.classList.toggle('visible');
+
+});
