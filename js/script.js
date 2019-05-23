3b56be245eb1f4fc4be4f5c1b02c80f0b1b22ae1
Martin Hartman <hartmanx1@wp.pl>
2019-05-23 16:59:54 +0200

    fix 13 errors

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
 var hamburger = document.querySelector('.toggle-menu');
 var sidebar = document.querySelector('.sidebar');
-var menager = document.querySelector('.menager');
-var michael = document.querySelector('.men');
-var ajax = document.querySelectorAll('.ajax');
-var test = document.querySelector('.michael');
-var logo = document.querySelector('.logo');
-var photo = document.querySelector('.photo');
-var icon = document.querySelectorAll('.icon');
-
 
 hamburger.addEventListener('click', function () {
-
-	for (var i = 0; i < ajax.length; i++) {
-		ajax[i].classList.toggle('visible');
-	};
-
-	for (var i = 0; i < icon.length; i++) {
-		icon[i].classList.toggle('visible');
-	};
-
-	test.classList.toggle('visible');
-	menager.classList.toggle('visible');
-	photo.classList.toggle('visible');
-	hamburger.classList.toggle('visible');
-	logo.classList.toggle('visible');
 	sidebar.classList.toggle('visible');
-	michael.classList.toggle('visible');
-
 });
